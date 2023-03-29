e6b91a6f879ee739f03c698587dd7fa727bcd751
U202110085 <u202110085@upc.edu.pe>
2023-03-28 23:43:09 -0500

    Agregar atributos dentro de Tutorial.cs

--- API/Models/Tutorial.cs
+++ API/Models/Tutorial.cs
@@ -1,6 +1,10 @@
 ﻿namespace API.Models;
+using System;
 
 public class Tutorial
 {
-    
+    public  int id { get; set; }
+    public string title { get; set; }
+    public string Description { get; set; }
+    public DateTime Year { get; set; }
 }
