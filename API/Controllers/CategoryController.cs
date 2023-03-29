adc06eeeecda46cd1f430eeaaa492de467288a37
U202110085 <u202110085@upc.edu.pe>
2023-03-29 00:23:38 -0500

    update controller

--- API/Controllers/CategoryController.cs
+++ API/Controllers/CategoryController.cs
@@ -12,30 +12,46 @@ namespace API.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private List<Category> categories;
+        public CategoryController()
+        {
+            categories = new List<Category>()
+            {
+                new Category(){id=1,Description = "nose", Quantity = 3},
+                new Category(){id=2,Description = "nose", Quantity = 3},
+                new Category(){id=3,Description = "nose", Quantity = 3},
+                new Category(){id=4,Description = "nose", Quantity = 3}
+            };
+        }
+        
         // GET: api/Category
         [HttpGet]
         public List<Tutorial> Get()
         {
-            Category category = new Category();
+            Category category = new Category()
+            {
+                id=1,name = "DIY", Quantity = 10,
+                Tutorials = new List<Tutorial>()
+                {
+                    new Tutorial(){id = 1, title = "Sopa", Description = "Cosas de comida", Year = new DateTime(2023, 3, 28, 16, 30, 0)}
+                }
+            };
             return category.getTutorials();
         }
-        
 
         // GET: api/Category/5
         [HttpGet("{id}", Name = "Get")]
-        public Category Get(int Id)
+        public Category Get(int id)
         {
-            Category category = new Category() {id = 1, name = "DIY", Description = "Realizar cosas por tu propia cuenta", Quantity= 23 };
-            if (category.id == Id)
+            for (int i = 0; i < categories.Count; i++)
             {
-                //StatusCode(500);
-                return category;
+                if (categories[i].id==id)
+                {
+                    return categories[i];
+                }
             }
-            else
-            {
-                //StatusCode(201);
-                return null;
-            }
+            
+            return null;
         }
 
         // POST: api/Category
